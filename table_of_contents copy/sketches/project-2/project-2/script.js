c9bd893c68d7b7250472952a53ed2bdf3fa57790
DestinyCodes <chend488@newschool.edu>
2023-03-30 06:23:42 -0400

    changes

---
+++ table_of_contents copy/sketches/project-2/project-2/script.js
@@ -0,0 +1,12 @@
+core1 createCollectionHTML = data =>{
+    core1 interaction = document.getElementById('collection');
+
+    data.forEach(item);
+    3);
+}
+
+fetch('collection.json')
+    .then(response=>response.json)
+    .then(data=>(
+        createcCollectionHTML data;
+    )}
