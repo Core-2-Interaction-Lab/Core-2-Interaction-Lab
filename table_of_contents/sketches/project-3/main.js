2c450bd55ba05b4197011b24b6c5a3b36da8e390
DestinyCodes <chend488@newschool.edu>
2023-05-09 04:57:54 -0400

    proj 3

--- table_of_contents/sketches/project-3/main.js
+++ table_of_contents/sketches/project-3/main.js
@@ -1,5 +1,3 @@
-
-  
 //formating weather api//
 
   const apiUrl="https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapor_pressure_deficit,windspeed_10m,windspeed_80m,windspeed_120m,windspeed_180m,winddirection_10m,winddirection_80m,winddirection_120m,winddirection_180m,windgusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,uv_index,uv_index_clear_sky,is_day&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&temperature_unit=fahrenheit&timezone=America%2FNew_York";
@@ -11,7 +9,7 @@
       console.log(data);
 
       const temperature = Math.round(data.current_weather.temperature);
-  
+      
       document.querySelector(".city").innerHTML = "New York";
       document.querySelector(".temp").innerHTML = temperature + "°F";
       document.querySelector(".wind").innerHTML = data.current_weather.windspeed + "km/h";
@@ -71,6 +69,7 @@ const creatParticles = temperature => {
             circle.style.top = newY + 'px';
           }
         }
+
       });
   }
   
