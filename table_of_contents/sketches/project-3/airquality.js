4df5d39a5cc7d410577f04c684b1b21afc611a02
DestinyCodes <chend488@newschool.edu>
2023-05-11 09:43:45 -0400

    Update airquality.js

--- table_of_contents/sketches/project-3/airquality.js
+++ table_of_contents/sketches/project-3/airquality.js
@@ -1,1 +1,21 @@
+// Make API request to the first URL
+fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapor_pressure_deficit,windspeed_10m,windspeed_80m,windspeed_120m,windspeed_180m,winddirection_10m,winddirection_80m,winddirection_120m,winddirection_180m,windgusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,uv_index,uv_index_clear_sky,is_day&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&temperature_unit=fahrenheit&timezone=America%2FNew_York"')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data from the first API
+    console.log(data);
 
+    // Make API request to the second URL
+    fetch('https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.5235&longitude=13.4115&hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,aerosol_optical_depth,dust,uv_index,uv_index_clear_sky,ammonia,alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen,us_aqi,us_aqi_pm2_5,us_aqi_pm10,us_aqi_no2,us_aqi_co,us_aqi_o3,us_aqi_so2&domains=cams_global&timezone=America%2FNew_York&past_days=1')
+      .then(response => response.json())
+      .then(data => {
+        // Process the data from the second API
+        console.log( data);
+
+        // Continue with your logic here
+        // You can access the data from both APIs and perform any necessary operations
+      })
+      .catch(error => {
+        console.log( error);
+      });
+  })
